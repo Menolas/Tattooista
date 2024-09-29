54391caf1961bc3fe2c79421fce099dbf128c5b3
Elena Christensen <olenachristensen777@gmail.com>
2024-09-29 22:05:29 +0300

    https://trello.com/c/cjr8ONUJ/14-add-ability-to-share-pages-in-social-media

--- Client/src/pages/mainPage/MainPage.tsx
+++ Client/src/pages/mainPage/MainPage.tsx
@@ -91,18 +91,6 @@ useEffect(() => {
 
   return (
     <>
-    <Helmet>
-        <title>{WEB_APP_TITLE}</title>
-        <meta name="description" content={`${WEB_APP_DESCRIPTION}`} />
-        <meta property="og:url" content={`${baseURL}/`} />
-        <meta property="og:title" content={`${WEB_APP_TITLE}`} />
-        <meta property="og:description" content={`${WEB_APP_DESCRIPTION}`} />
-        <meta property="og:image" content={`${baseURL}/uploads/facebookFlyer.png`} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={`${WEB_APP_TITLE}`} />
-        <meta name="twitter:description" content={`${WEB_APP_DESCRIPTION}`} />
-        <meta name="twitter:image" content={`${baseURL}/uploads/instagramFlyer.png`} />
-    </Helmet>
       <MainOffer />
         {
             isFetching
